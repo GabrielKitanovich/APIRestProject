c6861f5fb389e4fd6147dee28a72c7d93dcd991b
Gabriel <gabriel.kitanovich@gmail.com>
2024-09-19 22:22:35 -0300

    Project base

--- README.md
+++
@@ -1,2 +0,0 @@
-# APIRestProject
-First APIRestProject
---
+++ src/main/java/org/example/entities/Persona.java
@@ -0,0 +1,32 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.hibernate.envers.Audited;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "persona")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EnableJpaAuditing
+@Audited
+public class Persona implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "apellido")
+    private String apellido;
+    @Column(name = "dni")
+    private int dni;
+}
