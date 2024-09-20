8f01da78698583c5395a37cc6425e38723421ea7
Gabriel <gabriel.kitanovich@gmail.com>
2024-09-20 11:52:18 -0300

    First service and repository

---
+++ src/main/java/org/example/services/PersonaService.java
@@ -0,0 +1,32 @@
+package org.example.services;
+
+import org.example.entities.Persona;
+
+import java.util.List;
+
+public class PersonaService implements BaseService<Persona> {
+    @Override
+    public List<Persona> findAll() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public Persona findById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Persona save(Persona entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Persona update(Long id, Persona entity) throws Exception {
+        return null;
+    }
+
+    @Override
+    public boolean delete(Long id) throws Exception {
+        return false;
+    }
+}
